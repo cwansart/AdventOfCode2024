09d4836bb7db0066606afe5d0f60e3ac078b6675
Christian Wansart <cwansart@users.noreply.github.com>
2024-12-01 10:44:43 +0100

    clean up some things

--- Day1/Main.cpp
+++ Day1/Main.cpp
@@ -5,10 +5,11 @@
 #include <algorithm>
 #include <cmath>
 #include <stdexcept>
+#include <cstdint>
 
-constexpr size_t len = 1000;
+constexpr uint16_t len = 1000;
 
-static void ReadNumbers(const std::filesystem::path& file, std::array<int, len>& lNumbers, std::array<int, len>& rNumbers)
+static void ReadNumbers(const std::filesystem::path& file, std::array<uint32_t, len>& lNumbers, std::array<uint32_t, len>& rNumbers)
 {
     std::ifstream in{ file };
     if (!in)
@@ -16,8 +17,8 @@ static void ReadNumbers(const std::filesystem::path& file, std::array<int, len>&
         throw std::runtime_error("failed to open file: " + file.string());
     }
 
-    int lNum, rNum;
-    size_t n = 0;
+    uint32_t lNum, rNum;
+    uint16_t n = 0;
     while (in >> lNum >> rNum)
     {
         lNumbers[n] = lNum;
@@ -26,28 +27,28 @@ static void ReadNumbers(const std::filesystem::path& file, std::array<int, len>&
     }
 }
 
-static unsigned int Part1(std::array<int, len>& lNumbers, std::array<int, len>& rNumbers)
+static uint32_t Part1(std::array<uint32_t, len>& lNumbers, std::array<uint32_t, len>& rNumbers)
 {
     std::sort(lNumbers.begin(), lNumbers.end());
     std::sort(rNumbers.begin(), rNumbers.end());
 
-    unsigned int dist = 0;
-    for (size_t n = 0; n < len; n++)
+    uint32_t dist = 0;
+    for (uint16_t n = 0; n < len; n++)
     {
-        dist += std::abs(lNumbers[n] - rNumbers[n]);
+        dist += (uint32_t)std::abs((int64_t)lNumbers[n] - rNumbers[n]);
     }
 
     return dist;
 }
 
-static unsigned int Part2(const std::array<int, len>& lNumbers, const std::array<int, len>& rNumbers)
+static uint32_t Part2(const std::array<uint32_t, len>& lNumbers, const std::array<uint32_t, len>& rNumbers)
 {
-    unsigned int similarity = 0;
-    for (size_t n = 0; n < len; n++)
+    uint32_t similarity = 0;
+    for (uint16_t n = 0; n < len; n++)
     {
         auto lNum = lNumbers[n];
         auto count = std::count(rNumbers.begin(), rNumbers.end(), lNum);
-        similarity += lNum * count;
+        similarity += (uint32_t)(lNum * count);
     }
     return similarity;
 }
@@ -63,7 +64,7 @@ int main(int argc, char* argv[])
 
     try
     {
-        std::array<int, len> lNumbers{}, rNumbers{};
+        std::array<uint32_t, len> lNumbers{}, rNumbers{};
         ReadNumbers(file, lNumbers, rNumbers);
         auto dist = Part1(lNumbers, rNumbers);
         auto similarity = Part2(lNumbers, rNumbers);
@@ -74,5 +75,6 @@ int main(int argc, char* argv[])
     catch (const std::exception& e)
     {
         std::cerr << e.what() << std::endl;
+        return -1;
     }
 }
